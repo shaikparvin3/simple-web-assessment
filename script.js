a92e7912910226ed74bc70061a2fb9110cbaec40
shaik parvin <parvinshaik9080@gmail.com>
2024-06-28 17:00:39 +0530

    task updated

---
+++ script.js
@@ -0,0 +1,50 @@
+let a=document.createElement("header");
+a.textContent="software monks";
+document.body.appendChild(a);
+
+let b=document.createElement("main");
+document.body.appendChild(b);
+let c=document.createElement("h1");
+c.textContent="parvin";
+b.appendChild(c);
+
+let d=document.createElement("p");
+d.textContent=`this is parvin,i am from nellore,nellore dist,ap state
+        i was completed graducation in computer at kavali`;
+b.appendChild(d);
+
+let e=document.createElement("ul");
+b.appendChild(e);
+let f=document.createElement("li");
+f.textContent="my hobbie is cooking";
+e.appendChild(f);
+let g=document.createElement("li");
+g.textContent="learning myself";
+e.appendChild(g);
+let h=document.createElement("li");
+h.textContent="play shattle";
+e.appendChild(h);
+
+let i=document.createElement("img");
+i.src="https://cdn2.iconfinder.com/data/icons/vivid/48/image-512.png"
+i.alt="";
+b.appendChild(i);
+
+let j=document.createElement("footer");
+document.body.appendChild(j);
+let k=document.createElement("button");
+k.textContent="click";
+j.appendChild(k);
+
+
+let temp=document.querySelector("button");
+
+
+
+
+
+
+
+
+
+
